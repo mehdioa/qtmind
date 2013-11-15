84087e04820f2e7647a3cf9ed1ca8c4f08407494
Mehdi Omidali <mehdioa@gmail.com>
2013-11-15 17:41:04 +0330

    Initial commit

--- src/board.h
+++ src/board.h
@@ -1,6 +1,6 @@
 /***********************************************************************
  *
- * Copyright (C) 2013 Mehdi Omidal <mehdioa@gmail.com>
+ * Copyright (C) 2013 Omid Nikta <omidnikta@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
