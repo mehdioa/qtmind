c85a3865415b2114ebc1e2933eca86452627f792
Omid Nikta <omidnikta@gmail.com>
2014-04-11 16:57:13 +0430

    build-all script

---
+++ qtmind_build_all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd ../pkgbuild
+
+mkdir -p win64/qtmind-build
+cd win64/qtmind-build
+x86_64-w64-mingw32-qmake ../../../QtMind
+make clean
+make
+cp release/qtmind.exe ../
+cd ../..
+
+mkdir -p win32/qtmind-build
+cd win32/qtmind-build
+i686-w64-mingw32-qmake ../../../QtMind
+make clean
+make
+cp release/qtmind.exe ../
+cd ../..
+
+rm -r android
+mkdir android
+cd android
+/opt/android-qt5/5.2.0/bin/qmake ../../QtMind/
+make install INSTALL_ROOT=android-build/
+/opt/android-qt5/5.2.0/bin/androiddeployqt --output android-build/ --sign ../../../android_release.keystore nikta --input android-libqtmind.so-deployment-settings.json
+
+cd ..
