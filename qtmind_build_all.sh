00073b9908170b5aa7f794129daaee7175c4dced
Omid Nikta <omidnikta@gmail.com>
2014-04-17 17:27:46 +0430

    version 0.8.2-new

--- qtmind_build_all.sh
+++ qtmind_build_all.sh
@@ -2,19 +2,19 @@
 
 cd ../pkgbuild
 
-mkdir -p arch
-cp -u ../QtMind/AUR/*.* arch/
-cd arch
-find . -name qtmind\*.xz -delete
-makepkg -s
-linux32 makepkg -src --config ~/.makepkg.i686.conf
-cd ../
+# mkdir -p arch
+# cp -u ../QtMind/AUR/*.* arch/
+# cd arch
+# find . -name qtmind\*.xz -delete
+# makepkg -s
+# linux32 makepkg -src --config ~/.makepkg.i686.conf
+# cd ../
 
 
-# fpm -s dir -t deb -a "i386" -n "qtmind" --category 'Qt,Game,BoardGame' --deb-field 'Architecture:amd64'  --deb-field 'Depends: libqtgui4' --description 'A Mastermind game in Qt, with built in solver. Most of the major solving algorithms are integrated in QtMind.' --deb-field 'Provides:qtmind' --url 'http://github.com/omidnikta/qtmind' --license GPL2 --vendor '' -v 0.8.1-1 -m 'Omid Nikta' usr
+# fpm -s dir -t deb -a "i386" -n "qtmind" --category 'Qt,Game,BoardGame' --deb-field 'Depends: libqtgui4' --description 'A Mastermind game in Qt, with built in solver. Most of the major solving algorithms are integrated in QtMind.' --deb-field 'Provides:qtmind' --url 'http://github.com/omidnikta/qtmind' --license GPL2 --vendor '' -v 0.8.1-1 -m 'Omid Nikta' usr
 # 
 # 
-# fpm -s dir -t deb -a "amd64" -n "qtmind" --category 'Qt,Game,BoardGame' --deb-field 'Architecture:amd64'  --deb-field 'Depends: libqtgui4' --description 'A Mastermind game in Qt, with built in solver. Most of the major solving algorithms are integrated in QtMind.' --deb-field 'Provides:qtmind' --url 'http://github.com/omidnikta/qtmind' --license GPL2 --vendor '' -v 0.8.1-1 -m 'Omid Nikta' usr
+# fpm -s dir -t deb -a "amd64" -n "qtmind" --category 'Qt,Game,BoardGame'   --deb-field 'Depends: libqtgui4' --description 'A Mastermind game in Qt, with built in solver. Most of the major solving algorithms are integrated in QtMind.' --deb-field 'Provides:qtmind' --url 'http://github.com/omidnikta/qtmind' --license GPL2 --vendor '' -v 0.8.1-1 -m 'Omid Nikta' usr
 
 
 
@@ -34,11 +34,9 @@ make
 cp release/qtmind.exe ../
 cd ../..
 
-rm -r android
-mkdir android
-cd android
-/opt/android-qt5/5.2.0/bin/qmake ../../QtMind/
-make install INSTALL_ROOT=android-build/
-/opt/android-qt5/5.2.0/bin/androiddeployqt --output android-build/ --sign ../../../android_release.keystore nikta --input android-libqtmind.so-deployment-settings.json
-
-cd ..
+# rm -r android
+# mkdir android
+# cd android
+# /opt/android-qt5/5.2.0/bin/qmake ../../QtMind/
+# make install INSTALL_ROOT=android-build/
+# /opt/android-qt5/5.2.0/bin/androiddeployqt --output android-build/ --sign ../../../android_release.keystore nikta --input android-libqtmind.so-deployment-settings.json
