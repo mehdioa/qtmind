ae3e53f2e26b24fd349b58d55aaf274f629ea335
Omid Nikta <omidnikta@gmail.com>
2014-04-25 00:39:42 +0430

    build script revised

--- qtmind_build_all.sh
+++ qtmind_build_all.sh
@@ -2,41 +2,96 @@
 
 cd ../pkgbuild
 
-# mkdir -p arch
-# cp -u ../QtMind/AUR/*.* arch/
-# cd arch
-# find . -name qtmind\*.xz -delete
-# makepkg -s
+mkdir -p arch
+
+mkdir -p arch/src
+
+cp -u ../QtMind/AUR/* arch/
+
+cd arch
+
+find . -name qtmind\*.xz -delete
+
+find . -name qtmind\*.rpm -delete
+
+find . -name qtmind\*.deb -delete
+
+rm -r usr/
+
+cd src
+	if [ -d master ] ; then
+		cd master && git pull origin master && cd ..
+		msg "The local files are updated."
+	else
+		git clone ../../../QtMind --single-branch master
+	fi
+
+cd ..
+
+makepkg -s
 # linux32 makepkg -src --config ~/.makepkg.i686.conf
-# cd ../
 
+mv qtmind*.xz qtmind.xz
 
-# fpm -s dir -t deb -a "i386" -n "qtmind" --category 'Qt,Game,BoardGame' --deb-field 'Depends: libqtgui4' --description 'A Mastermind game in Qt, with built in solver. Most of the major solving algorithms are integrated in QtMind.' --deb-field 'Provides:qtmind' --url 'http://github.com/omidnikta/qtmind' --license GPL2 --vendor '' -v 0.8.1-1 -m 'Omid Nikta' usr
-# 
-# 
-# fpm -s dir -t deb -a "amd64" -n "qtmind" --category 'Qt,Game,BoardGame'   --deb-field 'Depends: libqtgui4' --description 'A Mastermind game in Qt, with built in solver. Most of the major solving algorithms are integrated in QtMind.' --deb-field 'Provides:qtmind' --url 'http://github.com/omidnikta/qtmind' --license GPL2 --vendor '' -v 0.8.1-1 -m 'Omid Nikta' usr
+tar xf qtmind.xz
 
+rm .INSTALL .MTREE .PKGINFO qtmind.xz
 
+fpm -s dir -t deb -a "amd64" -n "qtmind" --category 'Qt,Game,BoardGame' --deb-field  'Depends: libqtgui4' --description 'A Mastermind game in Qt, with built in solver. Most of the major solving algorithms are integrated in QtMind.' --deb-field 'Provides:qtmind' --url  'http://github.com/omidnikta/qtmind' --license GPL2 --vendor '' -v $1 -m 'Omid Nikta' usr
+
+mv -f qtmind*_amd64.deb ../../gh-pages/Downloads/qtmind_amd64.deb
+
+fpm -s dir -t rpm -n "qtmind" --category 'Qt,Game,BoardGame' --description 'A Mastermind game in Qt, with built in solver. Most of the major solving algorithms are integrated in QtMind.'  --url 'http://github.com/omidnikta/qtmind' --license GPL2 --vendor '' -v $1 -m 'Omid Nikta' -d 'libqt4' usr
+
+mv -f qtmind-*.x86_64.rpm ../../gh-pages/Downloads/qtmind_suse.x86_64.rpm
+
+fpm -s dir -t rpm -n "qtmind" --category 'Qt,Game,BoardGame' --description 'A Mastermind game in Qt, with built in solver. Most of the major solving algorithms are integrated in QtMind.'  --url 'http://github.com/omidnikta/qtmind' --license GPL2 --vendor '' -v $1 -m 'Omid Nikta' -d 'qt-x11' usr
+
+mv -f qtmind-*.x86_64.rpm ../../gh-pages/Downloads/qtmind_fedora.x86_64.rpm
+cd ..
+
+mkdir -p win64
 
 mkdir -p win64/qtmind-build
+
 cd win64/qtmind-build
+
 x86_64-w64-mingw32-qmake ../../../QtMind
+
 make clean
+
 make
+
 cp release/qtmind.exe ../
+
 cd ../..
+
+mkdir -p win32
 
 mkdir -p win32/qtmind-build
+
 cd win32/qtmind-build
+
 i686-w64-mingw32-qmake ../../../QtMind
+
 make clean
+
 make
+
 cp release/qtmind.exe ../
+
 cd ../..
 
-# rm -r android
-# mkdir android
-# cd android
-# /opt/android-qt5/5.2.0/bin/qmake ../../QtMind/
-# make install INSTALL_ROOT=android-build/
-# /opt/android-qt5/5.2.0/bin/androiddeployqt --output android-build/ --sign ../../../android_release.keystore nikta --input android-libqtmind.so-deployment-settings.json
+#rm -r android
+mkdir -p android
+
+cd android
+
+/opt/android-qt5/5.2.0/bin/qmake ../../QtMind/
+
+make install INSTALL_ROOT=android-build/
+
+/opt/android-qt5/5.2.0/bin/androiddeployqt --output android-build/ --sign ../../../android_release.keystore nikta --input android-libqtmind.so-deployment-settings.json
+
+mv -f android-build/bin/QtApp-release.apk ../../../../gh-pages/Downloads/QtMind.apk
+
